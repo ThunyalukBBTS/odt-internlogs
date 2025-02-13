30bd3c200cb0918683b8450ded7c2a6a4e4c2c30
thunyalukBBTS <thunyaluk.sa@kkumail.com>
2025-02-13 15:32:27 +0700

    convert to js stimulus for modal, add confirm add daily task modal

---
+++ app/javascript/controllers/newdailymodal_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="newdailymodal"
+export default class extends Controller {
+  static targets = ["background", "modal", "confirm_modal"]
+
+  connect() {
+  }
+  //  <%= @show_new_modal ? 'flex' : 'hidden' %>
+  show_modal() {
+    this.modalTarget.classList.remove("hidden")
+    this.modalTarget.classList.add("flex")
+    this.backgroundTarget.classList.add("blur-background")
+  }
+
+  close_modal() {
+    this.modalTarget.classList.remove("flex")
+    this.modalTarget.classList.add("hidden")
+    this.backgroundTarget.classList.remove("blur-background")
+  }
+
+  confirm() {
+    // close new modal
+    this.modalTarget.classList.remove("flex")
+    this.modalTarget.classList.add("hidden")
+    // show confirm modal 
+    this.confirm_modalTarget.classList.remove("hidden")
+    this.confirm_modalTarget.classList.add("flex")
+  }
+
+  back_to_edit() {
+    // close confirm modal
+    this.confirm_modalTarget.classList.remove("flex")
+    this.confirm_modalTarget.classList.add("hidden")
+    // show new modal 
+    this.modalTarget.classList.remove("hidden")
+    this.modalTarget.classList.add("flex")
+  }
+}
