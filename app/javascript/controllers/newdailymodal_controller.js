4034bec28cf052565c06568cf3401ca47d7460ad
thunyalukBBTS <thunyaluk.sa@kkumail.com>
2025-02-13 17:38:09 +0700

    finishing confirm modal and fix bug: db save accidently when click btn

--- app/javascript/controllers/newdailymodal_controller.js
+++ app/javascript/controllers/newdailymodal_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="newdailymodal"
 export default class extends Controller {
-  static targets = ["background", "modal", "confirm_modal"]
+  static targets = ["background", "modal", "confirm_modal", "input_date", "input_hours", "input_mins", "input_detail", "output_date", "output_hours", "output_mins", "output_detail", "form"]
 
   connect() {
   }
@@ -23,7 +23,11 @@ export default class extends Controller {
     // close new modal
     this.modalTarget.classList.remove("flex")
     this.modalTarget.classList.add("hidden")
-    // show confirm modal 
+    this.output_dateTarget.textContent = `${this.input_dateTarget.value}`
+    this.output_hoursTarget.textContent = `${this.input_hoursTarget.value}`
+    this.output_minsTarget.textContent = `${this.input_minsTarget.value}`
+    this.output_detailTarget.textContent = `${this.input_detailTarget.value}`
+    // show confirm modal
     this.confirm_modalTarget.classList.remove("hidden")
     this.confirm_modalTarget.classList.add("flex")
   }
@@ -32,8 +36,12 @@ export default class extends Controller {
     // close confirm modal
     this.confirm_modalTarget.classList.remove("flex")
     this.confirm_modalTarget.classList.add("hidden")
-    // show new modal 
+    // show new modal
     this.modalTarget.classList.remove("hidden")
     this.modalTarget.classList.add("flex")
+  }
+
+  submit() {
+    this.formTarget.submit()
   }
 }
