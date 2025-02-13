f5e9b49b378b89d7c3c3b12ba6d60f9a8e503eb8
kritsada653040438 <Kritsada.ma@kkumail.com>
2025-02-13 15:55:11 +0700

    Fix validates details to false and Maximum details length: 255

--- app/models/daily_task.rb
+++ app/models/daily_task.rb
@@ -2,5 +2,5 @@ class DailyTask < ApplicationRecord
   validates :date, presence: true
   validates :hours, presence: true, numericality: { only_integer: true }
   validates :mins, presence: true, numericality: { only_integer: true }
-  validates :detail, presence: true
+  validates :detail, presence: false
 end
