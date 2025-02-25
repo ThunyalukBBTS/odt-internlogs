9f7a47791c36f0d43bcff103eb17f8eb7984b456
sattrawoot_302004 <sattrawoot.p@kkumail.com>
2025-02-25 19:27:08 +0700

    add s to member

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@
     # Can be used by load balancers and uptime monitors to verify that the app is live.
     get "up" => "rails/health#show", as: :rails_health_check
     get "/my_daily_task", to: "tasks#index", as: "my_daily_task"
-    get "/odt_member", to: "members#index", as: "odt_member"
+    get "/odt_member", to: "member#index", as: "odt_member"
 
     # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
     # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
